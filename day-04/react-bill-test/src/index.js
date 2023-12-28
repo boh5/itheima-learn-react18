658a6e2fa8dee2a82d934d6388e4eab239690f93
boh5 <bohuang1994@gmail.com>
2023-12-28 21:22:35 +0800

    :sparkles: Complete craco config & jsconfig

--- day-04/react-bill-test/src/index.js
+++ day-04/react-bill-test/src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import sum from '@/test'
+
+console.log(sum(1, 3))
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
