8fa3535310ea95592a9aab2383de2700b56f1243
boh5 <bohuang1994@gmail.com>
2023-12-29 21:08:57 +0800

    :sparkles: 月度账单-账单按月分组

--- day-04/react-bill-test/src/pages/Month/index.js
+++ day-04/react-bill-test/src/pages/Month/index.js
@@ -1,10 +1,19 @@
 import {DatePicker, NavBar} from 'antd-mobile'
 import './index.scss'
-import {useState} from "react"
+import {useMemo, useState} from "react"
 import classNames from "classnames"
 import dayjs from "dayjs"
+import {useSelector} from "react-redux"
+import _ from "lodash"
 
 const Month = () => {
+  // 按月做数据分组
+  const billList = useSelector(state => state.bill.billList)
+  const monthGroup = useMemo(() => {
+    return _.groupBy(billList, (item) => dayjs(item.date).format('YYYY-MM'))
+  }, [billList]);
+  console.log(monthGroup)
+
   // 控制弹框的打开和关闭
   const [dateVisible, setDateVisible] = useState(false)
 
