fbc73b1569d189fcd9f5afd0802bab8ba4b6f888
boh5 <bohuang1994@gmail.com>
2023-12-29 21:39:50 +0800

    :sparkles: 月度账单-基于当前月的数据统计计算

--- day-04/react-bill-test/src/pages/Month/index.js
+++ day-04/react-bill-test/src/pages/Month/index.js
@@ -6,19 +6,40 @@ import dayjs from "dayjs"
 import {useSelector} from "react-redux"
 import _ from "lodash"
 
+const DATE_FORMAT = 'YYYY-MM'
+
 const Month = () => {
   // 按月做数据分组
   const billList = useSelector(state => state.bill.billList)
   const monthGroup = useMemo(() => {
-    return _.groupBy(billList, (item) => dayjs(item.date).format('YYYY-MM'))
+    return _.groupBy(billList, (item) => dayjs(item.date).format(DATE_FORMAT))
   }, [billList]);
-  console.log(monthGroup)
 
   // 控制弹框的打开和关闭
   const [dateVisible, setDateVisible] = useState(false)
 
   // 控制时间显示
   const [currentDate, setCurrentDate] = useState(() => new Date())
+
+  // 当前月数据
+  const [currentMonthList, setCurrentMonthList] = useState([])
+
+  const monthResult = useMemo(() => {
+    const pay = currentMonthList.filter(item => item.type === 'pay').reduce((a, c) => a + c.money, 0)
+    const income = currentMonthList.filter(item => item.type === 'income').reduce((a, c) => a + c.money, 0)
+    return {
+      pay,
+      income,
+      total: pay + income
+    }
+  }, [currentMonthList])
+
+  // 确认回调
+  const onConfirm = (date) => {
+    setCurrentDate(date)
+    const monthList = monthGroup[dayjs(date).format(DATE_FORMAT)]
+    setCurrentMonthList(monthList || [])
+  }
 
   return (
     <div className="monthlyBill">
@@ -30,22 +51,22 @@ const Month = () => {
           {/* 时间切换区域 */}
           <div className="date" onClick={() => setDateVisible(true)}>
             <span className="text">
-              {dayjs(currentDate).format('YYYY-MM')}月账单
+              {dayjs(currentDate).format(DATE_FORMAT)}月账单
             </span>
             <span className={classNames('arrow', dateVisible && 'expand')}></span>
           </div>
           {/* 统计区域 */}
           <div className='twoLineOverview'>
             <div className="item">
-              <span className="money">{100}</span>
+              <span className="money">{monthResult.pay.toFixed(2)}</span>
               <span className="type">支出</span>
             </div>
             <div className="item">
-              <span className="money">{200}</span>
+              <span className="money">{monthResult.income.toFixed(2)}</span>
               <span className="type">收入</span>
             </div>
             <div className="item">
-              <span className="money">{200}</span>
+              <span className="money">{monthResult.total.toFixed(2)}</span>
               <span className="type">结余</span>
             </div>
           </div>
@@ -56,7 +77,7 @@ const Month = () => {
             precision="month"
             visible={dateVisible}
             value={currentDate}
-            onConfirm={(date) => setCurrentDate(date)}
+            onConfirm={onConfirm}
             onClose={() => setDateVisible(false)}
             max={new Date()}
           />
