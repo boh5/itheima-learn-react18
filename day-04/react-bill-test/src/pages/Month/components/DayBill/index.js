8be64d4c8cf71941e50c4a5e3ed698e1f2034fec
boh5 <bohuang1994@gmail.com>
2023-12-30 21:47:09 +0800

    :sparkles: 月度账单-单日账单列表渲染

--- day-04/react-bill-test/src/pages/Month/components/DayBill/index.js
+++ day-04/react-bill-test/src/pages/Month/components/DayBill/index.js
@@ -1,6 +1,7 @@
 import classNames from 'classnames'
 import './index.scss'
 import {useMemo} from "react"
+import {billTypeToName} from "@/contants"
 
 const DailyBill = ({date, billList}) => {
   const dayResult = useMemo(() => {
@@ -34,6 +35,21 @@ const DailyBill = ({date, billList}) => {
             <span className="type">结余</span>
           </div>
         </div>
+      </div>
+      {/* 单日列表 */}
+      <div className="billList">
+        {billList.map(item => {
+          return (
+            <div className="bill" key={item.id}>
+              <div className="detail">
+                <div className="billType">{billTypeToName[item.useFor]}</div>
+              </div>
+              <div className={classNames('money', item.type)}>
+                {item.money.toFixed(2)}
+              </div>
+            </div>
+          )
+        })}
       </div>
     </div>
   )
