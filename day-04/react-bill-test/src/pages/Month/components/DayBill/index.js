9f710d021517549b39b555cc4248ee3a0478e0b0
boh5 <bohuang1994@gmail.com>
2023-12-30 21:54:03 +0800

    :sparkles: 月度账单-点击切换展开和收起

--- day-04/react-bill-test/src/pages/Month/components/DayBill/index.js
+++ day-04/react-bill-test/src/pages/Month/components/DayBill/index.js
@@ -1,6 +1,6 @@
 import classNames from 'classnames'
 import './index.scss'
-import {useMemo} from "react"
+import {useMemo, useState} from "react"
 import {billTypeToName} from "@/contants"
 
 const DailyBill = ({date, billList}) => {
@@ -14,12 +14,15 @@ const DailyBill = ({date, billList}) => {
     }
   }, [billList])
 
+  // 控制展开收起
+  const [visible, setVisible] = useState(false)
+
   return (
     <div className={classNames('dailyBill')}>
       <div className="header">
         <div className="dateIcon">
           <span className="date">{date}</span>
-          <span className={classNames('arrow')}></span>
+          <span className={classNames('arrow', visible && 'expand')} onClick={() => setVisible(!visible)}></span>
         </div>
         <div className="oneLineOverview">
           <div className="pay">
@@ -37,7 +40,7 @@ const DailyBill = ({date, billList}) => {
         </div>
       </div>
       {/* 单日列表 */}
-      <div className="billList">
+      <div className="billList" style={{display: visible ? 'block' : 'none'}}>
         {billList.map(item => {
           return (
             <div className="bill" key={item.id}>
