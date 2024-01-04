65345eb244b0133ceb51c4fd77783898551b2694
boh5 <bohuang1994@gmail.com>
2024-01-04 20:26:13 +0800

    :sparkles: Implement @ alias

--- day-12/react-jike-mobile/src/router/index.tsx
+++ day-12/react-jike-mobile/src/router/index.tsx
@@ -1,16 +1,16 @@
-import {createBrowserRouter} from "react-router-dom";
-import Home from "../pages/Home";
-import Detail from "../pages/Detail";
+import {createBrowserRouter} from 'react-router-dom'
+import Home from '@/pages/Home'
+import Detail from '@/pages/Detail'
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <Home />
+    element: <Home />,
   },
   {
     path: '/detail',
-    element: <Detail />
-  }
+    element: <Detail />,
+  },
 ])
 
 export {router}
